fd8927c71ece691d5d4e29b70dbe4fc6dc5f9a17
aksylumoed <aditya2697@gmail.com>
2018-01-17 13:12:50 +0530

    Masqueraded html as a php app

--- sudoku.js
+++ sudoku.js
@@ -1,5 +1,20 @@
-var timeline [];
+/* 1. Generate random solved grid.
+	  0. Naive Solver (to test)
+	  a. Using Dancing links
+   2. 
 
-var sudoku = {
-	
+
+*/
+
+/* NAIVE SOLVER */
+
+/*
+function solve(board, ind) {
+	var S = board.length;
+	if(ind = S*S){
+		// SOLUTION FOUND
+		
+
+	}
 }
+
