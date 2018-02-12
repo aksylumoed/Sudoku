113046bccd6879803d026a25d31e6a6fbee2c2b6
aksylumoed <aditya2697@gmail.com>
2018-02-12 12:11:07 +0100

    added instructions, 2x4 tables
    - images
    - instruction steps (commented)
    - 2x4 tables in array
    - Math.random()

--- timer.js
+++ timer.js
@@ -59,7 +59,7 @@ var Clock = {
         if (diff <= 0) {
             // add one second so that the count down starts at the full duration
             // example 05:00 not 04:59
-            start = Date.now() + 1000;
+            Clock.pause();
         }
     };
     timer();
@@ -80,7 +80,7 @@ pause: function() {
 
 
 window.onload = function () {
-    var twoMinutes = 60 * 2,
+    var twoMinutes = 60 * .2,
         display = document.querySelector('#time');
     Clock.startTimer(twoMinutes, display);
 };
