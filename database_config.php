c8405921f799203ce21df786eb771a4d4707ad41
aksylumoed <aditya2697@gmail.com>
2018-04-01 00:03:09 +0200

    data collection (php)

---
+++ database_config.php
@@ -0,0 +1,8 @@
+<?php
+  $servername = "eu-cdbr-west-02.cleardb.net";
+  $port = 3306;
+  $username = "bc476de262c8e0";
+  $password = "877cb939";
+  $dbname = "heroku_86d1ea2100652b1";
+  $table = "Users";
+?>
