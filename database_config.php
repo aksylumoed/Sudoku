719a190243e8fc894cc529b4c92b1d894c122597
aksylumoed <aditya2697@gmail.com>
2018-04-01 09:46:33 +0200

    changes php for basic file storage

--- database_config.php
+++ database_config.php
@@ -1,7 +1,7 @@
-
+<!-- 
 <?php
 
-$dbc = mysql_connect('eu-cdbr-west-02.cleardb.net', 'bc476de262c8e0', '877cb939');
-mysql_select_db('heroku_86d1ea2100652b1', $dbc);
+// $dbc = mysql_connect('eu-cdbr-west-02.cleardb.net', 'bc476de262c8e0', '877cb939');
+// mysql_select_db('heroku_86d1ea2100652b1', $dbc);
 
-?>
+?> -->
