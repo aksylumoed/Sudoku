9a6440930a830919f7f09863b17a4a3cb4fe1644
aksylumoed <adityadndkr@gmail.com>
2019-08-16 12:28:01 +0200

    test php script

--- qouta_read.php
+++ qouta_read.php
@@ -2,19 +2,18 @@
 include('database_config.php');
 $data_array = json_decode(file_get_contents('php://input'), true);
 
+// $response1 = 
+
 try {
     $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->prepare("SELECT responses FROM `$table_data`");
+    $stmt = $conn->prepare("SELECT responses FROM `$table_data` WHERE  `responses` = '{"Q0":"No, my family did not experience economic hardship"}'");
     $stmt->execute();
-    // $col_names = array();
-    // while($row = $stmt->fetchColumn()) {
-    //     $col_names[] = $row;
-    // }
+    
 
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    echo json_encode($result);
+    echo $result;
 
 }
 
