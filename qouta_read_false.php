1a6c1efbfe6ed777eb3c8803d0ce7db3c9d1a4a4
aksylumoed <adityadndkr@gmail.com>
2019-08-16 19:28:55 +0200

    added qouta_read

---
+++ qouta_read_false.php
@@ -0,0 +1,24 @@
+<?php
+include('database_config.php');
+// $data_array = json_decode(file_get_contents('php://input'), true);
+
+// $response1 = 
+
+try {
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("SELECT count(responses) FROM `$table_data` WHERE  `hardship` = false");
+    $stmt->execute();
+    $row = $stmt->fetch();
+
+    echo json_encode($row["count(responses)"]);
+
+}
+
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$conn = null;
+?> 
